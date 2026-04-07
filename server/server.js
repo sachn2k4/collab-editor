406740625d59acffbc9818b970e92cc350050d6b
Sachin Maurya <sachinmourya9821@gmail.com>
2026-04-08 02:45:32 +0530

    Final fix: root package.json for render

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const http = require('http');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const { Server } = require('socket.io');
+const path = require('path');
 const connectDB = require('./config/db');
 
 dotenv.config();
@@ -22,7 +23,18 @@ const io = new Server(server, {
 
 require('./sockets')(io);
 
-app.get('/', (req, res) => res.send('API is running...'));
+// Deployment Path Resolution
+if (process.env.NODE_ENV === 'production') {
+  // Serve frontend build output
+  app.use(express.static(path.join(__dirname, '../client/dist')));
+
+  // Catch-all route to serve React index.html for unknown routes
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  });
+} else {
+  app.get('/', (req, res) => res.send('API is running...'));
+}
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
