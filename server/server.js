28ba983cf2544af1e0da77908cd1543ce5f54e72
Sachin Maurya <sachinmourya9821@gmail.com>
2026-04-08 02:51:38 +0530

    Fix express wildcard crash

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/dist')));
 
   // Catch-all route to serve React index.html for unknown routes
-  app.get('*', (req, res) => {
+  app.use((req, res) => {
     res.sendFile(path.join(__dirname, '../client/dist/index.html'));
   });
 } else {
