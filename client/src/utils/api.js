ff7e14e1b9da32d8cf0700aa7daf7631a63ff63a
Sachin Maurya <sachinmourya9821@gmail.com>
2026-04-08 06:14:09 +0530

    Fix API Request Double Slashes

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 console.log("API URL:", import.meta.env.VITE_API_URL);
 
 const API = axios.create({
-  baseURL: (import.meta.env.VITE_API_URL || "http://localhost:5000") + "/api"
+  baseURL: (import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL.replace(/\/$/, '') : "http://localhost:5000") + "/api"
 });
 
 API.interceptors.request.use((config) => {
